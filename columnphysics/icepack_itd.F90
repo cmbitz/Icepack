503b309f4c22998b62fd155a576222bb1be149f3
Cecilia Bitz <bitz@uw.edu>
2026-02-09 17:53:11 -0800

    make hin_max(0)=0

--- columnphysics/icepack_itd.F90
+++ columnphysics/icepack_itd.F90
@@ -1666,7 +1666,7 @@
 
       if (kcatbound == -1) then ! single category
          hin_max(0) = c0
-         if (landfast) hin_max(0) = 0.2
+!         if (landfast) hin_max(0) = 0.2  ! unsuccessful since aicen fades away too slowly
          hin_max(1) = c100
 
       elseif (kcatbound == 0) then   ! original scheme
